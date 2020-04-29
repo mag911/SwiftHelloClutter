6ea5ac57550a272e17b29651a0076cba3da40051
mag911 <mag@nilskor.com>
2020-04-29 10:25:23 +0800

    Updated package.swift to mag911

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HelloClutter",
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftClutter.git", .branch("master")),
+        .package(url: "https://github.com/mag911/SwiftClutter.git", .branch("master")),
     ],
     targets: [
         .target(name: "HelloClutter", dependencies: ["Clutter"]),
